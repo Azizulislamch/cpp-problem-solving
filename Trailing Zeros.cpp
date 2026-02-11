e221f853db65ba18ad72a09026e2c1483e835863
Azizulislamch <azizulislamch@gmail.com>
2026-02-12 00:22:20 +0600

    added solution for Trailing Zeros

---
+++ Trailing Zeros.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+
+int TrailingZeros(int n){
+    int ans=0;
+    for(int i=5; i<=n; i*=5){
+        ans+=n/i;
+    }
+    return ans;
+}
+
+int main(){
+    fio();
+    int n; cin>>n;
+    cout<<TrailingZeros(n)<<endl;
+    return 0;
+}
