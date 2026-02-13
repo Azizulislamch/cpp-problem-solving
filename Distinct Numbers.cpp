67535674ea00a1c9ed73f827cc54537e7b10e82b
Azizulislamch <azizulislamch@gmail.com>
2026-02-13 23:49:07 +0600

    added solution for Distinct Numbers

---
+++ Distinct Numbers.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n; cin>>n;
+    set<int> s;
+    for(int i=0; i<n; i++){
+        int a; cin>>a;
+        s.insert(a);
+    } 
+    cout<<s.size()<<endl;
+}
+
+int main(){
+    fio();
+    solve();
+    return 0;
+}
