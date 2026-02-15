2170f343bf3fa45b2700ef20227ce115e86a1df0
Azizulislamch <azizulislamch@gmail.com>
2026-02-15 23:08:24 +0600

    added solution for Minimizing Coins

---
+++ Minimizing Coins.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n, x; cin>>n>>x;
+    vector<int> a(n);
+    for(int &c:a) cin>>c;
+ 
+    vector<int> dp(x+1, INF);
+    dp[0]=0;
+    
+    for(int c:a){
+        for(int i=c; i<=x; i++){
+            dp[i]=min(dp[i], dp[i-c]+1);
+        }
+    }
+    cout<<(dp[x]==INF ? -1:dp[x])<<endl;
+}
+
+int main(){
+    fio();
+    solve();
+    return 0;
+}
