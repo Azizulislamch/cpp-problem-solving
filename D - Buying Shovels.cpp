25cd4ca53a78606d309df84a0ef77f5b83d59f13
Azizulislamch <azizulislamch@gmail.com>
2026-03-27 00:53:28 +0600

    Add solution for D - Buying Shovels

---
+++ D - Buying Shovels.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    ll n, k; cin>>n>>k;
+    ll ans=n;
+    for(ll d=1; d*d<=n; d++){
+        if(n%d==0){
+            if(d<=k) ans=min(ans, n/d);
+            if(n/d<=k) ans=min(ans, d);
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main(){
+    fio();
+    TC solve();
+    return 0;
+}
