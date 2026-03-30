78fb13f080489645ebb9ef61a0bf65e6e5432ce1
Azizulislamch <azizulislamch@gmail.com>
2026-03-30 18:01:21 +0600

    Add solution for B - Left and Down

---
+++ B - Left and Down.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    ll a, b, k; cin>>a>>b>>k;
+ 
+    ll g=__gcd(a, b);
+    ll x=a/g;
+    ll y=b/g;
+ 
+    if(x<=k && y<=k){
+        cout<<1<<endl;
+    }else{
+        cout<<2<<endl;
+    }
+}
+
+int main(){
+    fio();
+    TC solve();
+    return 0;
+}
