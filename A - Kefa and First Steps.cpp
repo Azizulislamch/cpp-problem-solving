d6e19e1dbbf244d5c1400b068a2ad61c75953d6e
Azizulislamch <azizulislamch@gmail.com>
2026-03-31 19:38:56 +0600

    Add solution for A - Kefa and First Steps

---
+++ A - Kefa and First Steps.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n; cin>>n;
+    vector<int>a(n);
+    for(int i=0; i<n; i++) cin>>a[i];
+ 
+    int ans=0;
+    int cnt=1;
+    for(int i=1; i<n; i++){
+        if(a[i-1]<=a[i]) cnt++;
+        else{
+            ans=max(ans, cnt);
+            cnt=1;
+        }
+    }
+    ans=max(ans, cnt);
+    cout<<ans<<endl;
+}
+
+int main(){
+    fio();
+    solve();
+    return 0;
+}
