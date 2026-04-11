a0b8a32f6a422cb058e95f8cc6dd86a137e1390e
Azizulislamch <azizulislamch@gmail.com>
2026-04-11 20:28:13 +0600

    Add solution for B - Broken Keyboard

---
+++ B - Broken Keyboard.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n; cin>>n;
+    string s; cin>>s;
+
+    bool valid=(n%3!=2);
+    for(int i=0; i<n/3; i++){
+        if(s[i*3+1]!=s[i*3+2]){
+            valid=false;
+            break;
+        }
+    }
+    
+    if(valid){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+}
+
+int main(){
+    fio();
+    TC solve();
+    return 0;
+}
