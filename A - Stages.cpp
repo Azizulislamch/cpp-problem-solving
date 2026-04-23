548759b1c702e1e3e8f4927816367276bde42030
Azizulislamch <azizulislamch@gmail.com>
2026-04-23 23:27:49 +0600

    Add solution for A - Stages

---
+++ A - Stages.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n, k;
+    cin>>n>>k;
+    string s; cin>>s;
+ 
+    sort(s.begin(), s.end());
+ 
+    int ans=0;
+    int l='a'-2;
+    int cnt=0;
+    for(char c:s){
+        if(c>=l+2){
+            ans+=(c-'a'+1);
+            l=c;
+            cnt++;
+        }
+        if(cnt==k){
+            break;
+        }
+    }
+    if(cnt==k) cout<<ans<<endl;
+    else cout<<-1<<endl;
+}
+
+int main(){
+    fio();
+    solve();
+    return 0;
+}
