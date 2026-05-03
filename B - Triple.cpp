1d3b446b7c29a500523860169739ff7d23400551
Azizulislamch <azizulislamch@gmail.com>
2026-05-03 17:05:16 +0600

    Add solution for B - Triple

---
+++ B - Triple.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n; cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++) cin>>a[i];
+    sort(a, a+n);
+ 
+    int cnt=0;
+    int x=a[0];
+    for(int i=0; i<n; i++){
+        if(a[i]==x){
+            cnt++;
+            if(cnt==3) break;
+        }else{
+            cnt=1;
+            x=a[i];
+        }
+    }
+    if(cnt==3) cout<<x<<endl;
+    else cout<<-1<<endl;
+}
+
+int main(){
+    fio();
+    TC solve();
+    return 0;
+}
