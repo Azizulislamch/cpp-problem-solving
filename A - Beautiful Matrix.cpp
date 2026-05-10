6d25381f5dd8bb5aeedc2ccdc7cdd2273cf57168
Azizulislamch <azizulislamch@gmail.com>
2026-05-10 23:59:16 +0600

    Add solution for A - Beautiful Matrix

---
+++ A - Beautiful Matrix.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int a[5][5];
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            cin>>a[i][j];
+            if(a[i][j]==1){
+                cout<<abs(2-i)+abs(2-j)<<endl;
+            }
+        }
+    }
+}
+
+int main(){
+    fio();
+    solve();
+    return 0;
+}
