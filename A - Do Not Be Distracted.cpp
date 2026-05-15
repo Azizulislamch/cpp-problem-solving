92935f40c514159b1a460b1a42655db6bcae0a97
Azizulislamch <azizulislamch@gmail.com>
2026-05-15 21:47:16 +0600

    Add solution for A - Do Not Be Distracted!

---
+++ A - Do Not Be Distracted.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n; cin>>n;
+    string s, s1="";
+    cin>>s;
+    s1+=s[0];
+    for(int i=1; i<n; i++){
+        if(s[i]!=s[i-1]) s1+=s[i];
+    }
+    int flag=0;
+    for(int i=0; i<s1.size(); i++){
+        for(int j=i+1; j<s1.size(); j++){
+            if(s1[i]==s1[j]){
+                flag=1;
+                break;
+            }
+        }
+    }
+    if(flag==0){
+        cout<<"YES\n";
+    }else{
+        cout<<"NO\n";
+    }
+}
+
+int main(){
+    fio();
+    TC solve();
+    return 0;
+}
