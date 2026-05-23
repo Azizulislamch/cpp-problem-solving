1b6e7ae9cb0c08d76e12c5c942f6f12364e428c6
Azizulislamch <azizulislamch@gmail.com>
2026-05-23 11:27:01 +0600

    Add solution for B - T-primes

---
+++ B - T-primes.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+int prime(int x){
+    if(x==2){
+        return 1;
+    }
+    else if(x%2==0 || x==1){
+        return 0;
+    }
+    for(int i=3; i<=sqrt(x); i+=2){
+        if(x%i==0){
+            return 0;
+        }
+    }
+    return 1;
+}
+ 
+int main(){
+    ll n,x;
+    cin>>n;
+    while(n--){
+        cin>>x;
+        ll a=sqrt(x);
+        if(a*a==x && prime(a)){
+            cout<<"YES"<<endl;
+            continue;
+        }
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
