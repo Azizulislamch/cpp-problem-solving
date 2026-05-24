84e5a5c80b6e9a0f796eafb0eb437346521591b9
Azizulislamch <azizulislamch@gmail.com>
2026-05-24 20:40:11 +0600

    Add solution for B-Ten Words of Wisdom

---
+++ B - Ten Words of Wisdom.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int n, a, b;
+    cin>>n;
+    int m=0, j=0;
+    for(int i=1; i<=n; i++){
+        cin>>a>>b;
+        if(a<=10 && b>m){
+            m=b;
+            j=i;
+        }
+    }
+    cout<<j<<endl;
+}
+
+int main(){
+    fio();
+    TC solve();
+    return 0;
+}
