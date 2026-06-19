fb449db8f5a285fa6309d751f0cea56dbd9e8c3c
Azizulislamch <azizulislamch@gmail.com>
2026-06-19 20:57:44 +0600

    Add solution for A - Dislike of Threes

---
+++ A - Dislike of Threes.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define TC int tc; cin>>tc; for(int testcase=1; testcase<=tc; testcase++)
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define ui unsigned int
+#define ull unsigned long long
+#define pb push_back
+#define pi acos(-1)
+#define MAX 1000005
+#define INF 1e9
+#define MOD 998244353
+#define BITSCOUNT 32
+#define debug(x) cerr << x << endl;
+#define setBit(x, i) ((x) | (1LL << (i)))
+#define clearBit(x, i) ((x) & (~(1LL << (i))))
+#define checkBit(x, i) ((x) & (1LL << (i)))
+const int mod=1e9+7;
+
+
+void solve(){
+    int a;
+    cin>>a;
+    for(int i=1; ; i++){
+        if(i%3==0 or i%10==3){
+            continue;
+        }else{
+            a-=1;
+            if(a==0){
+                cout<<i<<endl;
+                break;
+            }
+        }
+    }
+}
+
+int main(){
+    fio();
+    TC solve();
+    return 0;
+}
